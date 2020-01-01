cf25af68a2b181d546694081c38e9b6dd18fb611
Rahul Mitra <rahulmitra980@gmail.com>
2020-01-01 21:28:27 +0530

    Updates

---
+++ precache-manifest.34f54f098130340311a715dd7c5f61be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa009b4830daa736f0399a98737d819",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b449a98f57b58600d8b5",
+    "url": "/portfolio/static/js/2.f508c738.chunk.js"
+  },
+  {
+    "revision": "90d6dfed0d84c3d8d55b",
+    "url": "/portfolio/static/js/main.ba5ef56e.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
